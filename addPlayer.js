6db48deddf72bbe1b7485617073dd7c7d4f1add4
CirJohne <john.cirverius@agical.se>
2015-08-19 19:41:49 +0200

    Center text. Readonly score. Focus all onclick. Changed x to playerName

--- addPlayer.js
+++ addPlayer.js
@@ -7,11 +7,13 @@ function addPlayer() {
     newPlayerNr = playerNr++;
 
 
-   var x = document.createElement("INPUT");
-   x.setAttribute("id", "field");
-   x.setAttribute("type", "text");
-   x.setAttribute("value", "Player "+ newPlayerNr);
-   document.body.appendChild(x);
+   var playerName = document.createElement("INPUT");
+   playerName.setAttribute("id", "field");
+   playerName.setAttribute("size", "20px");
+   playerName.setAttribute("type", "text");
+   playerName.setAttribute("value", "Player "+ newPlayerNr);
+   playerName.setAttribute("onclick","this.select()");
+   document.body.appendChild(playerName);
 
    var id_score = "score" + newPlayerNr;
    console.log(id_score);
@@ -26,13 +28,13 @@ function addPlayer() {
     var minuset = document.createElement("BUTTON");
     var minusetText = document.createTextNode("-1");
     minuset.onclick = function() {minus(id_score);}
-    // minuset.addEventListener("click", minus(id_score));
-    // minuset.setAttribute("onClick", "minus(id_score)");
     minuset.appendChild(minusetText);
     document.body.appendChild(minuset);
 
     var scoreField = document.createElement("INPUT");
     scoreField.setAttribute("id", id_score);
+    scoreField.setAttribute("readonly", "readonly");
+    scoreField.setAttribute("size", "4");
     scoreField.setAttribute("type", "text");
     scoreField.setAttribute("value", 0);
     document.body.appendChild(scoreField);
