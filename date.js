b4b7b9d9cbe6c9a92af545a5b2816fc7d58c59f6
CirJohne <john.cirverius@agical.se>
2015-08-21 18:55:25 +0200

    git rm some .js

--- date.js
+++
@@ -1,7 +0,0 @@
-//Present date and time in paragraph
-
-var rightNow = new Date();
-var p = document.createElement('p');
-var date = document.createTextNode(rightNow);
-p.appendChild(date);
-document.body.appendChild(p);
