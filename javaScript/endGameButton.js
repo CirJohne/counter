da814e3d74b4954f9dfd78463ff731eb1a5118c8
CirJohne <john.cirverius@agical.se>
2015-08-26 21:22:25 +0200

    createObject with keys and values

--- javaScript/endGameButton.js
+++ javaScript/endGameButton.js
@@ -1,3 +1,12 @@
+//Add a button called "End Game" which takes the player to another page (endGame.html)
+
+var endGameButton = document.createElement("BUTTON");
+var endGameText = document.createTextNode("End game");
+endGameButton.id = endGameButton;
+endGameButton.onclick = function(){createObject();};
+endGameButton.appendChild(endGameText);
+document.getElementById('endDiv').appendChild(endGameButton);
+
 //The array currentPlayers contains all the id's of current players.
 var currentPlayers = [];
 
@@ -5,36 +14,14 @@ var currentPlayers = [];
 var playersAndScores =[];
 
 //Create a hash with playername as keys and scores as values.
-function createHash(){
+function createObject(){
   for (var i = 0; i < currentPlayers.length; i++){
     var a = currentPlayers[i];
     scoreField = a.slice(5,7);
     currentScoreField = "score" + scoreField;
     console.log(currentScoreField);
     var b = parseInt(document.getElementById(currentScoreField).value);
-    playersAndScores[a] = b;
+    playersAndScores.push({key:a, value: b});
     console.log(playersAndScores);
   }
 }
-
-
-
-// function createHash(){
-//       var a = document.getElementById("field1").value;
-//       var b = parseInt(document.getElementById("score1").value);
-//       var hash= [];
-//       hash[a] = b ;
-// console.log(currentPlayers);
-// console.log(hash);
-//                       }
-
-
-
-//Add a button called "End Game" which takes the player to another page (endGame.html)
-
-var endGameButton = document.createElement("BUTTON");
-var endGameText = document.createTextNode("End game");
-endGameButton.id = endGameButton;
-endGameButton.onclick = function(){createHash();};
-endGameButton.appendChild(endGameText);
-document.getElementById('endDiv').appendChild(endGameButton);
