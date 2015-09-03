cb610b0826006e807e36a6b245bf0df682156621
CirJohne <john.cirverius@agical.se>
2015-09-03 17:50:10 +0200

    specen

--- spec/addPlayerSpec.js
+++ spec/addPlayerSpec.js
@@ -1,10 +1,32 @@
 
 describe("addPlayerWithArgs", function(){
   it ("should add 7 elements to playerDiv", function(){
-      var playerDiv = jasmine.createSpyObj('playerDiv', ['appendChild'])
+      var playerDiv = jasmine.createSpyObj('playerDiv', ['appendChild']);
 
-      addPlayerWithArgs(playerDiv);
+      addPlayerWithArgs(playerDiv,
+        function(elementType){
+          return {
+            appendChild: function(){}
+          };
+        },
+        function(text){}
+      );
 
       expect(playerDiv.appendChild.calls.count()).toEqual(7);
   });
 });
+
+describe("createTextField", function(){
+  it ("should create a textfield", function(){
+    var textField = createTextField("field1",10,"title",0,"className");
+    expect(textField.id).toEqual("field1");
+    expect(textField.size).toEqual(10);
+  });
+});
+
+// describe("failedTest", function(){
+//   it ("should fail", function(){
+//
+//     expect(1).toEqual(10);
+//   });
+// });
